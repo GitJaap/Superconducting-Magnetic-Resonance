01bebf53bce1923b2ecc03a97eff98ca312c51ae
JaapLaptop <GitJaap@noreply.user.github.com>
2014-10-29 12:30:53 +0100

    some testing files

--- qtlab_code/project/main/main.py
+++ qtlab_code/project/main/main.py
@@ -3,5 +3,17 @@ Created on 29 okt. 2014
 
 @author: Jaap
 '''
-
+import os;
+from measurement.flread import FileReader
+from pylab import *
 print('main is started within QTLab!');
+
+flr = FileReader('..//resources//res01TopoHanger_pow-10.dat');
+readData = flr.readColumns([0,3],separator = ' ');
+
+readData[:,1] = readData[:,1] - np.mean(readData[:,1]);
+plot(readData[:,0],readData[:,1]);
+show();
+
+
+execfile(os.path.join(os.path.split(os.getcwd())[0],'algoritms/animateAlgoritms.py'));
