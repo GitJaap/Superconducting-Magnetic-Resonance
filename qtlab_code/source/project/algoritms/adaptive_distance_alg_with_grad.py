6f9cc1f120816f278c8770db85e92761d75dc38b
JaapLaptop <GitJaap@noreply.user.github.com>
2014-11-11 15:26:59 +0100

    added the 2nd order gradient in the norm of the adaptive-distance-alg, but does not seem to make a big difference

--- qtlab_code/source/project/algoritms/adaptive_distance_alg_with_grad.py
+++ qtlab_code/source/project/algoritms/adaptive_distance_alg_with_grad.py
@@ -30,7 +30,14 @@ def d(xVec, yVec):
     :returns dis[i] = distance between point i and i-1'''
     xVecNorm = xVec / (np.max(xVec) - np.min(xVec))
     yVecNorm = yVec / (np.max(yVec) - np.min(yVec))
-
+    #approximate a spline of the function if nPoints > 3 = k (order of spline)
+    if(len(xVec) > 3):
+        spline = intp.splrep(xVec, yVec, s = 2)
+        #approximate the second derivative from this function
+        grad2 = intp.splev(xVec, spline, 2)
+        gradNorm = grad2 / (np.max(grad2) - np.min(grad2))
+    else:
+        gradNorm = np.zeros(len(xVec))
     #calculate the distance norm taken the x-distance y-distance and 2nd order derivative in accoutn
-    dis = np.sqrt(np.diff(xVecNorm) ** 2 + np.diff(yVecNorm) ** 2)
+    dis = np.sqrt(np.diff(xVecNorm) ** 2 + np.diff(yVecNorm) ** 2 + np.diff(gradNorm) ** 2)
     return dis
